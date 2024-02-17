2e2faf3f41ae5dfb992159de874314a971367dac
aykut <96610819+mrjjmc@users.noreply.github.com>
2024-02-17 19:10:43 +0100

    Update StarkNetToken.sol

--- src/starkware/isd/solidity/StarkNetToken.sol
+++ src/starkware/isd/solidity/StarkNetToken.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.16;
 import "starkware/isd/third_party/open_zeppelin/access/AccessControl.sol";
 import "starkware/isd/third_party/open_zeppelin/token/ERC20/extensions/ERC20Votes.sol";
 
-string constant NAME = "StarkNet Token";
-string constant SYMBOL = "STRK";
+string constant NAME = "Zksync Token";
+string constant SYMBOL = "ZKS";
 
 contract StarkNetToken is ERC20Votes, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
